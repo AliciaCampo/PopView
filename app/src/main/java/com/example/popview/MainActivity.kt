244182cad71af6bd10b56fc689825ba5081b114e
AliciaCampo <145765516+AliciaCampo@users.noreply.github.com>
2024-11-14 13:50:46 +0100

    error per corregir al iniciar l'app

--- app/src/main/java/com/example/popview/MainActivity.kt
+++ app/src/main/java/com/example/popview/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.popview
 
+import android.content.Intent
+import android.os.Handler
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         //prova
+        //configurar un retraso de 5 segundos antes de pasar a la pantalla de Login
+        Handler().postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        },5000)
     }
 }
