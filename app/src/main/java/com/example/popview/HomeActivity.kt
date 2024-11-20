357bcf9116014d548ab37d88796f54ba377da022
AliciaCampo <145765516+AliciaCampo@users.noreply.github.com>
2024-11-20 18:11:17 +0100

    todo bien en activity Home

--- app/src/main/java/com/example/popview/HomeActivity.kt
+++ app/src/main/java/com/example/popview/HomeActivity.kt
@@ -14,12 +14,7 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         // Verificar si el contenedor del fragmento está vacío
-        if (savedInstanceState == null) {
-            // Cargar el fragmento usando el FragmentManager
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, barramenu()) // Añadimos el fragmento
-                .commit()
-        }
+
 
 
     }
