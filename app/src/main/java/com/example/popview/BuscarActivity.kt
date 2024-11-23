ca7865b063a050f67623f00de0c2f84810888aa2
JudithM05 <2023_judith.mera@iticbcn.cat>
2024-11-23 16:58:03 +0100

    Añadida pantalla de valoración del título

--- app/src/main/java/com/example/popview/BuscarActivity.kt
+++ app/src/main/java/com/example/popview/BuscarActivity.kt
@@ -1,6 +1,9 @@
 package com.example.popview
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,13 @@ class BuscarActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val imageButtonWadeYLogan = findViewById<ImageButton>(R.id.deadpoolylobezno)
+        imageButtonWadeYLogan.setOnClickListener(View.OnClickListener {
+            val intent = Intent(
+                this@BuscarActivity,
+                ValoracionTituloActivity::class.java
+            )
+            startActivity(intent)
+        })
     }
 }
