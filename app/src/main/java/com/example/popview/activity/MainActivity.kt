5c9d2455054e356596d8972d12cd8c646bbda9b4
AliciaCampo <145765516+AliciaCampo@users.noreply.github.com>
2025-02-23 18:58:23 +0100

    mod MainActivity

--- app/src/main/java/com/example/popview/activity/MainActivity.kt
+++ app/src/main/java/com/example/popview/activity/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.example.popview.activity
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.popview.R
 import com.example.popview.data.Titulo
@@ -18,202 +15,181 @@ import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
     private lateinit var progressBar: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         // Referencia al ProgressBar
-         progressBar = findViewById<ProgressBar>(R.id.progressBar)
-        //llamada a la API y se mide el tiempo
+        progressBar = findViewById(R.id.progressBar)
+
+        // Llamada a la API y se mide el tiempo
         lifecycleScope.launch {
             val tiempoCarga = verificarYCrearTitulosIniciales()
-            //ajuste de la barra de progreso
-            cargaBarraProgreso(tiempoCarga)
+            // Ajuste de la barra de progreso
+            cargarBarraProgreso(tiempoCarga)
         }
     }
+
     private suspend fun verificarYCrearTitulosIniciales(): Long {
         return withContext(Dispatchers.IO) {
             measureTimeMillis {
-                try{
+                try {
                     val api = PopViewAPI().API()
-                    val titulos = api.getAllTitols()
-                    if(titulos.isEmpty()){
-                        val titulosIniciales = listOf(
-                            Titulo(
-                                id = 1,
-                                imagen = "sabrina.jpg",
-                                nombre = "Sabrina",
-                                description = "Descripció de Sabrina",
-                                genero = "Fantasia",
-                                edadRecomendada = 12,
-                                platforms = listOf("Netflix", "Amazon Prime"),
-                                rating = 3.5f,
-                                comments = listOf()
-                            ),
-                            Titulo(
-                                id = 2,
-                                imagen = "strangerthingscuatro.jpg",
-                                nombre = "Stranger Things",
-                                description = "Una sèrie de misteri",
-                                genero = "Ciència Ficció",
-                                edadRecomendada = 16,
-                                platforms = listOf("Netflix"),
-                                rating = 4f,
-                                comments = listOf()
-                            ),
-                            Titulo(
-                                id = 3,
-                                imagen = "orange_is_the_new_black.jpg",
-                                nombre = "Orange is the new black",
-                                description = "Descripció de Orange is the new black",
-                                genero = "Drama",
-                                edadRecomendada = 18,
-                                platforms = listOf("Netflix"),
-                                rating = 4f,
-                                comments = listOf()
-                            ),
-                            Titulo(
-                                id = 4,
-                                imagen = "wednesdaymiercoles.jpg",
-                                nombre = "Miercoles",
-                                description = "Descripció de Miercoles",
-                                genero = "Comèdia",
-                                edadRecomendada = 12,
-                                platforms = listOf("Netflix"),
-                                rating = 4f,
-                                comments = listOf()
-                            ),
-                            Titulo(
-                                id = 5,
-                                imagen = "deadpoolylobezno.jpg",
-                                nombre = "Deadpool y Lobezno",
-                                description = "Descripció de Deadpool y Lobezno",
-                                genero = "Acció",
-                                edadRecomendada = 18,
-                                platforms = listOf("Netflix"),
-                                rating = 4f,
-                                comments = listOf()
-                            ),
-                            Titulo(
-                                id = 6,
-                                imagen = "delrevesdos.jpg",
-                                nombre = "Del Reves 2",
-                                description = "Descripció de Del Reves 2",
-                                genero = "Animació",
-                                edadRecomendada = 6,
-                                platforms = listOf("Disney"),
-                                rating = 4f,
-                                comments = listOf()
-                            ),
-                            Titulo(
-                                id = 7,
-                                imagen = "respira.jpg",
-                                nombre = "Respira",
-                                description = "Relaxa't i respira",
-                                genero = "Drama",
-                                edadRecomendada = 16,
-                                platforms = listOf("Netflix"),
-                                rating = 4f,
-                                comments = listOf()
-                            ),
-                            Titulo(
-                                id = 8,
-                                imagen = "beetlejuice2.jpg",
-                                nombre = "Beetlejuice 2",
-                                description = "Descripció de Beetlejuice 2",
-                                genero = "Comèdia",
-                                edadRecomendada = 12,
-                                platforms = listOf("Netflix"),
-                                rating = 4f,
-                                comments = listOf()
-                            ),
-                            Titulo(
-                                id = 9,
-                                imagen = "jokerdos.jpg",
-                                nombre = "Joker 2",
-                                description = "Descripció de Joker 2",
-                                genero = "Drama",
-                                edadRecomendada = 18,
-                                platforms = listOf("Netflix"),
-                                rating = 4f,
-                                comments = listOf()
-                            ),
-                            Titulo(
-                                id = 10,
-                                imagen = "juegodelcalamar.jpg",
-                                nombre = "Juego del Calamar",
-                                description = "Descripció de Juego del Calamar",
-                                genero = "Acció",
-                                edadRecomendada = 18,
-                                platforms = listOf("Netflix"),
-                                rating = 4f,
-                                comments = listOf()
-                            ),
-                            Titulo(
-                                id = 11,
-                                imagen = "robotsalvaje.jpg",
-                                nombre = "Robot Salvaje",
-                                description = "Descripció de Robot Salvaje",
-                                genero = "Ciència Ficció",
-                                edadRecomendada = 12,
-                                platforms = listOf("Netflix"),
-                                rating = 4f,
-                                comments = listOf()
-                            ),
-                            Titulo(
-                                id = 12,
-                                imagen = "venom3.jpg",
-                                nombre = "Venom 3",
-                                description = "Descripció de Venom 3",
-                                genero = "Acció",
-                                edadRecomendada = 16,
-                                platforms = listOf("Netflix"),
-                                rating = 4f,
-                                comments = listOf()
-                            ),
-                            Titulo(
-                                id = 13,
-                                imagen = "cobrakai.jpg",
-                                nombre = "Cobra Kai",
-                                description = "Descripció de Cobra Kai",
-                                genero = "Acció",
-                                edadRecomendada = 12,
-                                platforms = listOf("Netflix"),
-                                rating = 4f,
-                                comments = listOf()
-                            ),
-                            Titulo(
-                                id = 14,
-                                imagen = "casadepapel.jpg",
-                                nombre = "Casa de Papel",
-                                description = "Descripció de Casa de Papel",
-                                genero = "Acció",
-                                edadRecomendada = 16,
-                                platforms = listOf("Netflix"),
-                                rating = 4f,
-                                comments = listOf()
-                            )
+                    val titulosExistentes = api.getAllTitols()
+                    val titulosIniciales = listOf(
+                        Titulo(
+                            imagen = "sabrina.jpg",
+                            nombre = "Sabrina",
+                            description = "Descripción de Sabrina",
+                            genero = "Fantasía",
+                            edadRecomendada = 12,
+                            platforms = listOf("Netflix", "Amazon Prime"),
+                            rating = 3.5f
+                        ),
+                        Titulo(
+                            imagen = "strangerthings.jpg",
+                            nombre = "Stranger Things",
+                            description = "Una serie de misterio",
+                            genero = "Ciencia ficción",
+                            edadRecomendada = 16,
+                            platforms = listOf("Netflix"),
+                            rating = 4.0f
+                        ),
+                        Titulo(
+                            imagen = "orange_is_the_new_black.jpg",
+                            nombre = "Orange is the New Black",
+                            description = "Descripción de Orange is the New Black",
+                            genero = "Drama",
+                            edadRecomendada = 18,
+                            platforms = listOf("Netflix"),
+                            rating = 4.0f
+                        ),
+                        Titulo(
+                            imagen = "wednesday.jpg",
+                            nombre = "Wednesday",
+                            description = "Descripción de Wednesday",
+                            genero = "Comedia",
+                            edadRecomendada = 12,
+                            platforms = listOf("Netflix"),
+                            rating = 4.0f
+                        ),
+                        Titulo(
+                            imagen = "deadpool_and_wolverine.jpg",
+                            nombre = "Deadpool y Wolverine",
+                            description = "Descripción de Deadpool y Wolverine",
+                            genero = "Acción",
+                            edadRecomendada = 18,
+                            platforms = listOf("Netflix"),
+                            rating = 4.0f
+                        ),
+                        Titulo(
+                            imagen = "del_reves_2.jpg",
+                            nombre = "Del Revés 2",
+                            description = "Descripción de Del Revés 2",
+                            genero = "Animación",
+                            edadRecomendada = 6,
+                            platforms = listOf("Disney"),
+                            rating = 4.0f
+                        ),
+                        Titulo(
+                            imagen = "respira.jpg",
+                            nombre = "Respira",
+                            description = "Relax and breathe",
+                            genero = "Drama",
+                            edadRecomendada = 16,
+                            platforms = listOf("Netflix"),
+                            rating = 4.0f
+                        ),
+                        Titulo(
+                            imagen = "beetlejuice_2.jpg",
+                            nombre = "Beetlejuice 2",
+                            description = "Descripción de Beetlejuice 2",
+                            genero = "Comedia",
+                            edadRecomendada = 12,
+                            platforms = listOf("Netflix"),
+                            rating = 4.0f
+                        ),
+                        Titulo(
+                            imagen = "joker_2.jpg",
+                            nombre = "Joker 2",
+                            description = "Descripción de Joker 2",
+                            genero = "Drama",
+                            edadRecomendada = 18,
+                            platforms = listOf("Netflix"),
+                            rating = 4.0f
+                        ),
+                        Titulo(
+                            imagen = "juego_del_calamar.jpg",
+                            nombre = "Juego del Calamar",
+                            description = "Descripción de Juego del Calamar",
+                            genero = "Acción",
+                            edadRecomendada = 18,
+                            platforms = listOf("Netflix"),
+                            rating = 4.0f
+                        ),
+                        Titulo(
+                            imagen = "robot_salvaje.jpg",
+                            nombre = "Robot Salvaje",
+                            description = "Descripción de Robot Salvaje",
+                            genero = "Ciencia ficción",
+                            edadRecomendada = 12,
+                            platforms = listOf("Netflix"),
+                            rating = 4.0f
+                        ),
+                        Titulo(
+                            imagen = "venom_3.jpg",
+                            nombre = "Venom 3",
+                            description = "Descripción de Venom 3",
+                            genero = "Acción",
+                            edadRecomendada = 16,
+                            platforms = listOf("Netflix"),
+                            rating = 4.0f
+                        ),
+                        Titulo(
+                            imagen = "cobra_kai.jpg",
+                            nombre = "Cobra Kai",
+                            description = "Descripción de Cobra Kai",
+                            genero = "Acción",
+                            edadRecomendada = 12,
+                            platforms = listOf("Netflix"),
+                            rating = 4.0f
+                        ),
+                        Titulo(
+                            imagen = "casa_de_papel.jpg",
+                            nombre = "Casa de Papel",
+                            description = "Descripción de Casa de Papel",
+                            genero = "Acción",
+                            edadRecomendada = 16,
+                            platforms = listOf("Netflix"),
+                            rating = 4.0f
                         )
-                        for (titulo in titulosIniciales){
-                            api.createTitulo(titulo)
-                        }
+                    )
+
+                    val titulosACrear = titulosIniciales.filter { titulo ->
+                        titulosExistentes.none { it.nombre == titulo.nombre }
+                    }
+
+                    for (titulo in titulosACrear) {
+                        api.createTitulo(titulo)
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
                 }
-            }catch (e: Exception){
-                e.printStackTrace()
             }
-                }
         }
     }
-    //metodo que carga la barra de progreso
-    private suspend fun cargaBarraProgreso(tiempoCarga: Long){
-        val tiempoTotal= if(tiempoCarga<2000) 2000 else tiempoCarga
-        val interval =50
+
+    private suspend fun cargarBarraProgreso(tiempoCarga: Long) {
+        val tiempoTotal = if (tiempoCarga < 2000) 2000 else tiempoCarga
+        val interval = 50
         var progress = 0
-        withContext(Dispatchers.Main){
-            while(progress<=100){
+        withContext(Dispatchers.Main) {
+            while (progress <= 100) {
                 progressBar.progress = progress
                 progress += interval
                 delay(interval.toLong())
-                progress +=(100 * interval / tiempoTotal).toInt()
+                progress += (100 * interval / tiempoTotal).toInt()
             }
         }
     }
--- app/src/main/java/com/example/popview/data/Titulo.kt
+++ app/src/main/java/com/example/popview/data/Titulo.kt
@@ -1,13 +1,15 @@
 package com.example.popview.data
+
 import java.io.Serializable
+
 data class Titulo(
-    val id: Int,
     val imagen: String,          // Recurso de la imagen
     val nombre: String,               // Título del contenido
     val description: String, // Descripción del contenido
-    val genero : String, //genero titulo
+    val genero: String, // Género del título
     val edadRecomendada: Int,       // Edad recomendada
     val platforms: List<String>,     // Plataformas disponibles
     val rating: Float,               // Puntuación
-    val comments: List<Item>         // Lista de comentarios
+    val comments: List<Item> = listOf(), // Lista de comentarios
+    val id: Int = 0 // ID generado automáticamente por el servidor
 ) : Serializable
