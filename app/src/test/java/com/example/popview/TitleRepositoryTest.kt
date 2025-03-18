9a67b073c043428101b91ec2857a1a86d46fd61c
AliciaCampo <145765516+AliciaCampo@users.noreply.github.com>
2025-03-18 12:15:56 +0100

    test inicial creado

---
+++ app/src/test/java/com/example/popview/TitleRepositoryTest.kt
@@ -0,0 +1,13 @@
+package com.example.popview
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+class TitleRepositoryTest {
+    @Test
+    fun `cargar 120 títulos correctamente`() {
+        // Simulación de los títulos cargados (como si los devolviera el repositorio)
+        val fakeTitles = List(120) { "Título ${it + 1}" }
+        // Comprobamos que la lista tiene exactamente 120 elementos
+        assertEquals(120, fakeTitles.size)
+    }
+}
