2ee6560176c9bd5eea6fb69ca9fd27dee4c9e01e
AliciaCampo <145765516+AliciaCampo@users.noreply.github.com>
2025-03-21 21:11:35 +0100

    Intento de arreglar las dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,50 +40,48 @@ android {
 }
 
 dependencies {
-    implementation(libs.retrofit)
-    implementation (libs.converter.gson)
-    implementation ("com.github.bumptech.glide:glide:4.15.1")
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
-    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("androidx.recyclerview:recyclerview:1.2.0")
+    // Librerías de AndroidX
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("androidx.recyclerview:recyclerview:1.2.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+
+    // Retrofit y Gson
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    // Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.firestore)
+
+    // DataStore (corregido)
+    implementation(libs.androidx.datastore.preferences)
+
+    // Corrutinas
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    // MPAndroidChart
+    implementation(libs.mpandroidchart)
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
+
+    // Navegación
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    // Logging Interceptor
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+    // Tests
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
-    implementation ("com.google.android.material:material:1.8.0")
-    implementation ("androidx.cardview:cardview:1.0.0")
-    implementation ("com.google.code.gson:gson:2.8.9")
-
-    // Dependencias para corrutinas
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    //Graficos e informes
-    /*implementation(platform( libs.firebase.bom))
-
-    // Add the dependency for the Analytics library
-    // When using the BoM, you don't specify versions in Firebase library dependencies
-    implementation(libs.firebase.analytics)
-    implementation(libs.firebase.firestore)
-    implementation(libs.mpandroidchart)
-    implementation(libs.androidx.datastore.core.android)*/
-    implementation ("androidx.datastore:datastore-preferences:1.0.0")
-    implementation (platform("com.google.firebase:firebase-bom:32.3.1"))
-    implementation ("com.google.firebase:firebase-firestore-ktx")
-    // Import the BoM for the Firebase platform
-    implementation(platform( libs.firebase.bom))
-
-    // Add the dependency for the Analytics library
-    // When using the BoM, you don't specify versions in Firebase library dependencies
-    implementation(libs.firebase.analytics)
-    implementation(libs.firebase.firestore)
-    implementation(libs.mpandroidchart)
-    implementation("com.github.PhilJay:MPAndroidChart:3.1.0")
-
 }
+
