637538520674aaec08ec357dc2fbe3db7deab425
AliciaCampo <145765516+AliciaCampo@users.noreply.github.com>
2025-04-02 19:18:14 +0200

    Firebase actualizado para eliminar listas desde UsuarioActivity

--- app/src/main/java/com/example/popview/adapter/ListaAdapter.kt
+++ app/src/main/java/com/example/popview/adapter/ListaAdapter.kt
@@ -8,9 +8,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.example.popview.PopViewApp
 import com.example.popview.R
 import com.example.popview.data.Lista
 import com.example.popview.service.PopViewAPI
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -44,7 +47,7 @@ class ListasAdapter(
                     // Eliminar de la llista local i notificar a l'adapter
                     listas.removeAt(position)
                     notifyItemRemoved(position)
-
+                    registrarListaEliminada()
                     // Crida a l'API per eliminar la llista
                     CoroutineScope(Dispatchers.IO).launch {
                         try {
@@ -61,6 +64,18 @@ class ListasAdapter(
                 .show()
             true
         }
+    }
+    private fun registrarListaEliminada() {
+        val db = FirebaseFirestore.getInstance()
+        val deviceRef = db.collection("Devices").document(PopViewApp.idDispositiu)
+
+        deviceRef.update("listasEliminadas", FieldValue.increment(1))
+            .addOnSuccessListener {
+                Log.d("PopViewApp", "Lista registrado correctamente en Devices")
+            }
+            .addOnFailureListener { e ->
+                Log.e("PopViewApp", "Error al registrar lista en Devices", e)
+            }
     }
     override fun getItemCount(): Int = listas.size
 }
