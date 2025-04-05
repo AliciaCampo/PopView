d4f1a31ab3c5af4835cfde991d0ec1779296b7e2
AliciaCampo <145765516+AliciaCampo@users.noreply.github.com>
2025-04-05 18:07:21 +0200

    Usuario view model empezado

--- app/src/main/java/com/example/popview/viewmodel/UsuarioViewModel.kt
+++ app/src/main/java/com/example/popview/viewmodel/UsuarioViewModel.kt
@@ -1,2 +1,25 @@
 package com.example.popview.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
+
+data class EstadoUsuario(
+    var esValid: Boolean,
+    var errorNombreUsuario: String?,
+    var errorEmailUsuario: String?,
+    var errorEdadUsuario : Int,
+    var errorContrasenyaUsuario: String?,
+)
+
+class UsuarioViewModel : ViewModel() {
+
+    private var nombreUsuario: String = ""
+    private var emailUsuario: String = ""
+    private var edadUsuario: Int = 0
+    private var contrasenyaUsuario: String = ""
+
+    private val _validaciodades = MutableLiveData<EstadoUsuario>()
+    val validaciodades : LiveData<EstadoUsuario> = _validaciodades
+
+}
