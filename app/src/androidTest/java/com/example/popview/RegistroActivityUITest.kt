8f9ecdec82eecdada298f9a8bca8b2e0001a83db
JudithM05 <2023_judith.mera@iticbcn.cat>
2025-05-03 17:07:34 +0200

    Fent canvis RegistroActivityUITest

--- app/src/androidTest/java/com/example/popview/RegistroActivityUITest.kt
+++ app/src/androidTest/java/com/example/popview/RegistroActivityUITest.kt
@@ -16,16 +16,140 @@ import org.junit.runner.RunWith
 class RegistroActivityUITest {
 
     @get:Rule
-    var activityRule = ActivityScenarioRule(RegistroActivity::class.java)
+    val activityRule = ActivityScenarioRule<RegistroActivity>(RegistroActivity::class.java)
 
     @Test
-    fun testNombreUsuarioVacioMuestraError() {
-        onView(withId(R.id.textInputUsuario))
-            .perform(click(), clearText())
+    fun visualitzacioInicial() {
+        onView(withId(R.id.textInputUsuario)).check(matches(isDisplayed()))
+        onView(withId(R.id.textInputEmail)).check(matches(isDisplayed()))
+        onView(withId(R.id.textInputEdad)).check(matches(isDisplayed()))
+        onView(withId(R.id.textInputContrasenya)).check(matches(isDisplayed()))
+        onView(withId(R.id.textInputContrasenyaDos)).check(matches(isDisplayed()))
+        onView(withId(R.id.imagenAvatar)).check(matches(isDisplayed()))
+        onView(withId(R.id.buttonRegistre)).check(matches(isDisplayed()))
+    }
 
+    @Test
+    fun registreAmbDadesValides() {
+        onView(withId(R.id.textInputUsuario)).perform(typeText("Alicia"))
+        onView(withId(R.id.textInputEmail)).perform(typeText("alicia@gmail.com"))
+        onView(withId(R.id.textInputEdad)).perform(typeText("22"))
+        onView(withId(R.id.textInputContrasenya)).perform(typeText("Pass@1234"))
+        onView(withId(R.id.textInputContrasenyaDos)).perform(typeText("Pass@1234"))
+        onView(withId(R.id.imagenAvatar)).perform(click())
+        onView(withText("avataruser1.png")).perform(click())
         onView(withId(R.id.buttonRegistre)).perform(click())
+    }
 
-        onView(withId(R.id.textInputUsuario))
-            .check(matches(hasErrorText("El nom ha de tenir mínim 5 caràcters")))
+    @Test
+    fun nomInvalidCurt() {
+        onView(withId(R.id.textInputUsuario)).perform(typeText("Ali"))
+        onView(withId(R.id.textInputEmail)).perform(typeText("alicia@gmail.com"))
+        onView(withId(R.id.textInputEdad)).perform(typeText("22"))
+        onView(withId(R.id.textInputContrasenya)).perform(typeText("Pass@1234"))
+        onView(withId(R.id.textInputContrasenyaDos)).perform(typeText("Pass@1234"))
+        onView(withId(R.id.imagenAvatar)).perform(click())
+        onView(withText("avataruser1.png")).perform(click())
+        onView(withId(R.id.buttonRegistre)).perform(click())
+        onView(withId(R.id.textInputUsuario)).check(matches(hasErrorText("El nom ha de tenir mínim 5 caràcters")))
+    }
+
+    @Test
+    fun nomAmbCaractersNoPermesos() {
+        onView(withId(R.id.textInputUsuario)).perform(typeText("Alicia123"))
+        onView(withId(R.id.buttonRegistre)).perform(click())
+        onView(withId(R.id.textInputUsuario)).check(matches(hasErrorText("El nom només pot contenir lletres")))
+    }
+
+    @Test
+    fun emailInvalid() {
+        onView(withId(R.id.textInputEmail)).perform(typeText("user@mal"))
+        onView(withId(R.id.buttonRegistre)).perform(click())
+        onView(withId(R.id.textInputEmail)).check(matches(hasErrorText("El format de l'email no és vàlid")))
+    }
+
+    @Test
+    fun edatInvalidaMenor16() {
+        onView(withId(R.id.textInputUsuario)).perform(typeText("Alicia"))
+        onView(withId(R.id.textInputEmail)).perform(typeText("alicia@gmail.com"))
+        onView(withId(R.id.textInputEdad)).perform(typeText("15"))
+        onView(withId(R.id.textInputContrasenya)).perform(typeText("Pass@1234"))
+        onView(withId(R.id.textInputContrasenyaDos)).perform(typeText("Pass@1234"))
+        onView(withId(R.id.imagenAvatar)).perform(click())
+        onView(withText("avataruser1.png")).perform(click())
+        onView(withId(R.id.buttonRegistre)).perform(click())
+        onView(withId(R.id.textInputEdad)).check(matches(hasErrorText("Has de ser major de 16 anys")))
+    }
+
+    @Test
+    fun edatInvalidaNoNumerica() {
+        onView(withId(R.id.textInputUsuario)).perform(typeText("Alicia"))
+        onView(withId(R.id.textInputEmail)).perform(typeText("alicia@gmail.com"))
+        onView(withId(R.id.textInputEdad)).perform(typeText("abc"))
+        onView(withId(R.id.textInputContrasenya)).perform(typeText("Pass@1234"))
+        onView(withId(R.id.textInputContrasenyaDos)).perform(typeText("Pass@1234"))
+        onView(withId(R.id.imagenAvatar)).perform(click())
+        onView(withText("avataruser1.png")).perform(click())
+        onView(withId(R.id.buttonRegistre)).perform(click())
+        onView(withId(R.id.textInputEdad)).check(matches(hasErrorText("L'edat ha de ser un número")))
+    }
+
+    @Test
+    fun contrasenyaInvalidaSenseNumero() {
+        onView(withId(R.id.textInputUsuario)).perform(typeText("Alicia"))
+        onView(withId(R.id.textInputEmail)).perform(typeText("alicia@gmail.com"))
+        onView(withId(R.id.textInputEdad)).perform(typeText("22"))
+        onView(withId(R.id.textInputContrasenya)).perform(typeText("Contrasenya@"))
+        onView(withId(R.id.textInputContrasenyaDos)).perform(typeText("Contrasenya@"))
+        onView(withId(R.id.imagenAvatar)).perform(click())
+        onView(withText("avataruser1.png")).perform(click())
+        onView(withId(R.id.buttonRegistre)).perform(click())
+        onView(withId(R.id.textInputContrasenya)).check(matches(hasErrorText("La contrasenya ha de contenir al menys un número")))
+    }
+
+    @Test
+    fun confirmacioContrasenyaIncorrecta() {
+        onView(withId(R.id.textInputUsuario)).perform(typeText("Alicia"))
+        onView(withId(R.id.textInputEmail)).perform(typeText("alicia@gmail.com"))
+        onView(withId(R.id.textInputEdad)).perform(typeText("22"))
+        onView(withId(R.id.textInputContrasenya)).perform(typeText("Pass@1234"))
+        onView(withId(R.id.textInputContrasenyaDos)).perform(typeText("Otra@1234"))
+        onView(withId(R.id.imagenAvatar)).perform(click())
+        onView(withText("avataruser1.png")).perform(click())
+        onView(withId(R.id.buttonRegistre)).perform(click())
+        onView(withId(R.id.textInputContrasenyaDos)).check(matches(hasErrorText("Les contrasenyes no coincideixen")))
+    }
+
+    @Test
+    fun avatarInvalid() {
+        onView(withId(R.id.textInputUsuario)).perform(typeText("Alicia"))
+        onView(withId(R.id.textInputEmail)).perform(typeText("alicia@gmail.com"))
+        onView(withId(R.id.textInputEdad)).perform(typeText("22"))
+        onView(withId(R.id.textInputContrasenya)).perform(typeText("Pass@1234"))
+        onView(withId(R.id.textInputContrasenyaDos)).perform(typeText("Pass@1234"))
+        onView(withId(R.id.imagenAvatar)).perform(click())
+        onView(withText("imagenloca.png")).perform(click())
+        onView(withId(R.id.buttonRegistre)).perform(click())
+    }
+
+    @Test
+    fun seleccioAvatar() {
+        onView(withId(R.id.imagenAvatar)).perform(click())
+        onView(withText("avataruser2.png")).perform(click())
+        onView(withId(R.id.imagenAvatar)).check(matches(isDisplayed()))
+    }
+
+    // Mètode auxiliar per verificar errors en TextInputLayout
+    private fun hasErrorText(errorText: String): org.hamcrest.Matcher<android.view.View> {
+        return object : org.hamcrest.TypeSafeMatcher<android.view.View>() {
+            override fun matchesSafely(view: android.view.View): Boolean {
+                if (view !is com.google.android.material.textfield.TextInputLayout) return false
+                return view.error == errorText
+            }
+
+            override fun describeTo(description: org.hamcrest.Description) {
+                description.appendText("TextInputLayout with error: $errorText")
+            }
+        }
     }
 }
